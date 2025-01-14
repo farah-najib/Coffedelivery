f476eae6984143236e7afac4b50423cb5ea411d1
Farah Najib <farahengnajib@gmail.com>
2025-01-14 21:19:59 +0100

    update footer

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,8 +1,6 @@
-import Logo from '../assets/logo.png'
 
 import styled from 'styled-components'
 
-// Styled-components
 const FooterWrapper = styled.footer`
     position: relative;
     width: 1441px;
@@ -108,6 +106,262 @@ const TextWrapper50 = styled.div`
     font-style: var(--text-s-bold-font-style);
 `
 
+const Group62 = styled.div`
+    position: absolute;
+    width: 630px;
+    height: 257px;
+    top: 120px;
+    left: 161px;
+`
+
+const Group63 = styled.div`
+    position: absolute;
+    width: 261px;
+    height: 257px;
+    top: 0;
+    left: 0;
+`
+
+const Group64 = styled.div`
+    position: absolute;
+    width: 96px;
+    height: 66px;
+    top: 0;
+    left: 0;
+`
+
+const TextWrapper51 = styled.div`
+    position: absolute;
+    width: 257px;
+    top: 129px;
+    left: 0;
+    font-family: var(--text-s-regular-font-family);
+    font-weight: var(--text-s-regular-font-weight);
+    color: var(--base-100);
+    font-size: var(--text-s-regular-font-size);
+    letter-spacing: var(--text-s-regular-letter-spacing);
+    line-height: var(--text-s-regular-line-height);
+    font-style: var(--text-s-regular-font-style);
+`
+
+const IconoirInstagram2 = styled.img`
+    position: absolute;
+    width: 32px;
+    height: 32px;
+    top: 225px;
+    left: 0;
+`
+
+const IconoirTiktok = styled.img`
+    position: absolute;
+    width: 32px;
+    height: 32px;
+    top: 225px;
+    left: 48px;
+`
+
+const Group65 = styled.div`
+    position: absolute;
+    width: 32px;
+    height: 32px;
+    top: 225px;
+    left: 96px;
+`
+
+const OverlapGroup8 = styled.div`
+    position: relative;
+    width: 27px;
+    height: 19px;
+    top: 7px;
+    left: 3px;
+`
+
+const Vector2 = styled.img`
+    position: absolute;
+    width: 5px;
+    height: 5px;
+    top: 7px;
+    left: 11px;
+`
+
+const Vector3 = styled.img`
+    position: absolute;
+    width: 27px;
+    height: 19px;
+    top: 0;
+    left: 0;
+`
+
+const TextWrapper52 = styled.div`
+    position: absolute;
+    top: 82px;
+    left: 0;
+    font-family: var(--title-s-semibold-font-family);
+    font-weight: var(--title-s-semibold-font-weight);
+    color: #ffffff;
+    font-size: var(--title-s-semibold-font-size);
+    letter-spacing: var(--title-s-semibold-letter-spacing);
+    line-height: var(--title-s-semibold-line-height);
+    white-space: nowrap;
+    font-style: var(--title-s-semibold-font-style);
+`
+
+const Group66 = styled.div`
+    position: absolute;
+    width: 156px;
+    height: 125px;
+    top: 129px;
+    left: 482px;
+`
+
+const TextWrapper53 = styled.div`
+    top: 31px;
+    position: absolute;
+    left: 0;
+    font-family: var(--text-l-regular-font-family);
+    font-weight: var(--text-l-regular-font-weight);
+    color: var(--base-200);
+    font-size: var(--text-l-regular-font-size);
+    letter-spacing: var(--text-l-regular-letter-spacing);
+    line-height: var(--text-l-regular-line-height);
+    white-space: nowrap;
+    font-style: var(--text-l-regular-font-style);
+`
+
+const TextWrapper54 = styled.div`
+    top: 65px;
+    position: absolute;
+    left: 0;
+    font-family: var(--text-l-regular-font-family);
+    font-weight: var(--text-l-regular-font-weight);
+    color: var(--base-200);
+    font-size: var(--text-l-regular-font-size);
+    letter-spacing: var(--text-l-regular-letter-spacing);
+    line-height: var(--text-l-regular-line-height);
+    white-space: nowrap;
+    font-style: var(--text-l-regular-font-style);
+`
+
+const TextWrapper55 = styled.div`
+    position: absolute;
+    top: 99px;
+    left: 0;
+    font-family: var(--text-l-regular-font-family);
+    font-weight: var(--text-l-regular-font-weight);
+    color: var(--base-200);
+    font-size: var(--text-l-regular-font-size);
+    letter-spacing: var(--text-l-regular-letter-spacing);
+    line-height: var(--text-l-regular-line-height);
+    white-space: nowrap;
+    font-style: var(--text-l-regular-font-style);
+`
+
+const TextWrapper56 = styled.div`
+    top: 0;
+    position: absolute;
+    left: 0;
+    font-family: var(--text-l-regular-font-family);
+    font-weight: var(--text-l-regular-font-weight);
+    color: var(--base-200);
+    font-size: var(--text-l-regular-font-size);
+    letter-spacing: var(--text-l-regular-letter-spacing);
+    line-height: var(--text-l-regular-line-height);
+    white-space: nowrap;
+    font-style: var(--text-l-regular-font-style);
+`
+
+const Group67 = styled.div`
+    position: absolute;
+    width: 134px;
+    height: 175px;
+    top: 82px;
+    left: 289px;
+`
+
+const TextWrapper57 = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    font-family: var(--title-s-semibold-font-family);
+    font-weight: var(--title-s-semibold-font-weight);
+    color: var(--basewhite);
+    font-size: var(--title-s-semibold-font-size);
+    letter-spacing: var(--title-s-semibold-letter-spacing);
+    line-height: var(--title-s-semibold-line-height);
+    white-space: nowrap;
+    font-style: var(--title-s-semibold-font-style);
+`
+
+const TextWrapper58 = styled.div`
+    top: 47px;
+    position: absolute;
+    left: 0;
+    font-family: var(--text-l-regular-font-family);
+    font-weight: var(--text-l-regular-font-weight);
+    color: var(--base-200);
+    font-size: var(--text-l-regular-font-size);
+    letter-spacing: var(--text-l-regular-letter-spacing);
+    line-height: var(--text-l-regular-line-height);
+    white-space: nowrap;
+    font-style: var(--text-l-regular-font-style);
+`
+
+const TextWrapper59 = styled.div`
+    position: absolute;
+    top: 81px;
+    left: 0;
+    font-family: var(--text-l-regular-font-family);
+    font-weight: var(--text-l-regular-font-weight);
+    color: var(--base-200);
+    font-size: var(--text-l-regular-font-size);
+    letter-spacing: var(--text-l-regular-letter-spacing);
+    line-height: var(--text-l-regular-line-height);
+    white-space: nowrap;
+    font-style: var(--text-l-regular-font-style);
+`
+
+const TextWrapper60 = styled.div`
+    position: absolute;
+    top: 115px;
+    left: 0;
+    font-family: var(--text-l-regular-font-family);
+    font-weight: var(--text-l-regular-font-weight);
+    color: var(--base-200);
+    font-size: var(--text-l-regular-font-size);
+    letter-spacing: var(--text-l-regular-letter-spacing);
+    line-height: var(--text-l-regular-line-height);
+    white-space: nowrap;
+    font-style: var(--text-l-regular-font-style);
+`
+
+const TextWrapper61 = styled.div`
+    position: absolute;
+    top: 149px;
+    left: 0;
+    font-family: var(--text-l-regular-font-family);
+    font-weight: var(--text-l-regular-font-weight);
+    color: var(--base-200);
+    font-size: var(--text-l-regular-font-size);
+    letter-spacing: var(--text-l-regular-letter-spacing);
+    line-height: var(--text-l-regular-line-height);
+    white-space: nowrap;
+    font-style: var(--text-l-regular-font-style);
+`
+
+const Text = styled.div`
+    position: absolute;
+    top: 454px;
+    left: 155px;
+    font-family: var(--text-l-regular-font-family);
+    font-weight: var(--text-l-regular-font-weight);
+    color: #000000;
+    font-size: var(--text-l-regular-font-size);
+    letter-spacing: var(--text-l-regular-letter-spacing);
+    line-height: var(--text-l-regular-line-height);
+    white-space: nowrap;
+    font-style: var(--text-l-regular-font-style);
+`
+
 const Footer = () => {
     return (
         <FooterWrapper>
@@ -124,9 +378,34 @@ const Footer = () => {
                             <TextWrapper50>Text button</TextWrapper50>
                         </AtomPrimaryButton2>
                     </Group61>
-                    {/* More components here, refactor the rest the same way */}
+                    <Group62>
+                        <Group63>
+                            <Group64>
+                                <TextWrapper51>Follow us</TextWrapper51>
+                                <IconoirInstagram2 src="instagram-icon.png" />
+                                <IconoirTiktok src="tiktok-icon.png" />
+                            </Group64>
+                        </Group63>
+                        <Group65>
+                            <OverlapGroup8>
+                                <Vector2 src="vector2.png" />
+                                <Vector3 src="vector3.png" />
+                            </OverlapGroup8>
+                        </Group65>
+                    </Group62>
+                    <Group66>
+                        <TextWrapper52>Contact Us</TextWrapper52>
+                        <Group67>
+                            <TextWrapper57>Location</TextWrapper57>
+                            <TextWrapper58>Email</TextWrapper58>
+                            <TextWrapper59>Phone</TextWrapper59>
+                            <TextWrapper60>Address</TextWrapper60>
+                            <TextWrapper61>Website</TextWrapper61>
+                        </Group67>
+                    </Group66>
                 </Overlap12>
             </OrganismFooter>
+            <Text>Footer text goes here</Text>
         </FooterWrapper>
     )
 }
