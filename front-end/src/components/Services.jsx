320d7ab88cfd4a5a40117f15b52433f28e4dfaa3
Farah Najib <farahengnajib@gmail.com>
2025-01-14 21:14:07 +0100

    add design to service component

--- front-end/src/components/Services.jsx
+++ front-end/src/components/Services.jsx
@@ -1,19 +1,314 @@
 import Calender from '../assets/calendar.svg'
 import Shop from '../assets/shop.svg'
 import DeliveryTruck from '../assets/delivery-truck.svg'
+import styled from 'styled-components'
+const OurServices = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh; /* Full viewport height */
+    // overflow: hidden;
+    background-color: palevioletred;
+    background-size: cover;
+    background-position: 50% 50%;
+`
+
+const Group57 = styled.div`
+    position: relative;
+    width: 1122px;
+    height: 370px;
+
+`
+
+const Group58 = styled.div`
+    position: absolute;
+    width: 356px;
+    height: 248px;
+    top: 122px;
+    left: 0;
+`
+
+const Group59 = styled.div`
+    position: absolute;
+    width: 356px;
+    height: 248px;
+    top: 122px;
+    left: 384px;
+`
+
+const Group60 = styled.div`
+    position: absolute;
+    width: 356px;
+    height: 248px;
+    top: 122px;
+    left: 768px;
+`
+
+const OverlapGroup = styled.div`
+    position: relative;
+    width: 352px;
+    height: 248px;
+    background-color: #ffffff;
+    border-radius: 8px;
+    box-shadow: var(--drop-2);
+`
+
+const Title = styled.div`
+    position: absolute;
+    top: 88px;
+    left: 127px;
+    font-family: var(--title-s-semibold-font-family);
+    font-weight: var(--title-s-semibold-font-weight);
+    color: #000000;
+    font-size: var(--title-s-semibold-font-size);
+    letter-spacing: var(--title-s-semibold-letter-spacing);
+    line-height: var(--title-s-semibold-line-height);
+    white-space: nowrap;
+    font-style: var(--title-s-semibold-font-style);
+`
+
+const Description = styled.p`
+    position: absolute;
+    width: 320px;
+    top: 127px;
+    left: 16px;
+    font-family: var(--text-s-regular-font-family);
+    font-weight: var(--text-s-regular-font-weight);
+    color: var(--base-600);
+    font-size: var(--text-s-regular-font-size);
+    text-align: center;
+    letter-spacing: var(--text-s-regular-letter-spacing);
+    line-height: var(--text-s-regular-line-height);
+    font-style: var(--text-s-regular-font-style);
+`
+
+const Button = styled.div`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    gap: 4px;
+    padding: 12px 24px;
+    position: absolute;
+    top: 179px;
+    left: 73px;
+    border-radius: 8px;
+    border: 1px solid var(--green-600);
+`
+
+const ButtonText = styled.div`
+    color: var(--green-600);
+    font-size: var(--text-s-bold-font-size);
+    line-height: var(--text-s-bold-line-height);
+    font-family: var(--text-s-bold-font-family);
+    font-weight: var(--text-s-bold-font-weight);
+    letter-spacing: var(--text-s-bold-letter-spacing);
+    white-space: nowrap;
+    font-style: var(--text-s-bold-font-style);
+`
+
+const Image = styled.img`
+    position: absolute;
+    width: 32px;
+    height: 32px;
+    top: 16px;
+    left: 16px;
+`
+
+
+const CheckOurProducts = styled.div`
+    position: relative;
+
+    width: 1120px;
+    height: 241px;
+    border: 1px solid;
+    border-color: #ffffff1a;
+    background-color: lightsalmon;
+    background-size: 100% 100%;
+`
+
+const Overlap13 = styled.div`
+    position: absolute;
+    width: 816px;
+    height: 177px;
+    top: 32px;
+    left: 256px;
+`
+
+const Group68 = styled.div`
+    position: absolute;
+    width: 565px;
+    height: 62px;
+    top: 0;
+    left: 227px;
+`
+
+const TextWrapper65 = styled.div`
+    width: 563px;
+    left: 0;
+    font-size: var(--title-XL-semibold-font-size);
+    line-height: var(--title-XL-semibold-line-height);
+    position: absolute;
+    top: 0;
+    font-family: var(--title-XL-semibold-font-family);
+    font-weight: var(--title-XL-semibold-font-weight);
+    color: #ffffff;
+    letter-spacing: var(--title-XL-semibold-letter-spacing);
+    font-style: var(--title-XL-semibold-font-style);
+`
+
+const AtomCTAButton3 = styled.div`
+    top: 136px;
+    left: 227px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    padding: 10px 24px;
+    position: absolute;
+    background-color: var(--basewhite);
+    border-radius: 8px;
+`
+
+const TextButton6 = styled.div`
+    position: relative;
+    width: fit-content;
+    margin-top: -1px;
+    font-family: var(--text-m-bold-font-family);
+    font-weight: var(--text-m-bold-font-weight);
+    color: var(--blue-900);
+    font-size: var(--text-m-bold-font-size);
+    letter-spacing: var(--text-m-bold-letter-spacing);
+    line-height: var(--text-m-bold-line-height);
+    white-space: nowrap;
+    font-style: var(--text-m-bold-font-style);
+`
+
+const UpgradeYourCoffee = styled.p`
+    position: absolute;
+    width: 589px;
+    top: 69px;
+    left: 227px;
+    font-family: var(--text-m-regular-font-family);
+    font-weight: var(--text-m-regular-font-weight);
+    color: #ffffff;
+    font-size: var(--text-m-regular-font-size);
+    letter-spacing: var(--text-m-regular-letter-spacing);
+    line-height: var(--text-m-regular-line-height);
+    font-style: var(--text-m-regular-font-style);
+`
+
+const Group69 = styled.img`
+    position: absolute;
+    width: 245px;
+    height: 148px;
+    top: 2px;
+    left: 0;
+`
+
+const Group70 = styled.div`
+    position: absolute;
+    width: 243px;
+    height: 283px;
+    top: -21px;
+    left: 13px;
+`
+
+const OverlapGroup9 = styled.div`
+    position: relative;
+    height: 283px;
+`
+
+const VectaryTexture = styled.img`
+    position: absolute;
+    width: 144px;
+    height: 283px;
+    top: 0;
+    left: 0;
+`
+
+const VectaryTexture2 = styled.img`
+    position: absolute;
+    width: 126px;
+    height: 190px;
+    top: 93px;
+    left: 117px;
+`
+
 const Services = () => {
     return (
-        <div className="services">
-            #Our services Schedule Planning an office party or a coffee break
-            with friends? Let us take care of everything. Schedule our service
-            <img className="img-2" src={Calender} />
-            Find a store near you Stores Find the nearest one and come try a new
-            experience and taste for yourself.
-            <img className="img-2" src={Shop} />
-            <img className="img-2" src={DeliveryTruck} />
-            Delivery With our platform, we can deliver coffee straight to you,
-            no matter where you are. Order coffee now
-        </div>
+        <OurServices>
+            <Group57>
+                <div>#Our services</div>
+
+                <Group58>
+                    <OverlapGroup>
+                        <Title>Schedule</Title>
+                        <Description>
+                            Planning an office party or a coffee break with
+                            friends? Let us take care of everything.
+                        </Description>
+                        <Button>
+                            <ButtonText>Schedule our service</ButtonText>
+                        </Button>
+                        <Image src={Calender} />
+                    </OverlapGroup>
+                </Group58>
+
+                <Group59>
+                    <OverlapGroup>
+                        <Title>Stores</Title>
+                        <Description>
+                            Find the nearest one and come try a new experience
+                            and taste for yourself.
+                        </Description>
+                        <Button>
+                            <ButtonText>Find a store near you</ButtonText>
+                        </Button>
+                        <Image src={Shop} />
+                    </OverlapGroup>
+                </Group59>
+
+                <Group60>
+                    <OverlapGroup>
+                        <Title>Delivery</Title>
+                        <Description>
+                            With our platform, we can deliver coffee straight to
+                            you, no matter where you are.
+                        </Description>
+                        <Button>
+                            <ButtonText>Order coffee now</ButtonText>
+                        </Button>
+                        <Image src={DeliveryTruck} />
+                    </OverlapGroup>
+                </Group60>
+            </Group57>
+
+            <CheckOurProducts>
+                <Overlap13>
+                    <Group68>
+                        <TextWrapper65>Upgrade your experience</TextWrapper65>
+                    </Group68>
+                    <AtomCTAButton3>
+                        <TextButton6>Try our fresh beans</TextButton6>
+                    </AtomCTAButton3>
+                    <UpgradeYourCoffee>
+                        Upgrade your coffee experience. Try our fresh beans and
+                        taste the rich, delicious flavour for yourself. You
+                        won&#39;t be disappointed.
+                    </UpgradeYourCoffee>
+                    <Group69 src="https://c.animaapp.com/EXuBewV4/img/group-51-1@2x.png" />
+                </Overlap13>
+                <Group70>
+                    <OverlapGroup9>
+                        <VectaryTexture src="https://c.animaapp.com/EXuBewV4/img/vectary-texture@2x.png" />
+                        <VectaryTexture2 src="https://c.animaapp.com/EXuBewV4/img/vectary-texture-1@2x.png" />
+                    </OverlapGroup9>
+                </Group70>
+            </CheckOurProducts>
+        </OurServices>
     )
 }
 
