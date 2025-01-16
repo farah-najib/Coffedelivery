a9d0ac8124eb61bb9b419a83f402e30fc5134db1
Farah Najib <farahengnajib@gmail.com>
2025-01-16 13:46:11 +0100

    refactor product component

--- front-end/src/components/Products.jsx
+++ front-end/src/components/Products.jsx
@@ -11,19 +11,10 @@ const OurProducts = styled.div`
     background-size: cover;
     background-position: 50% 50%;
 `
+const HeaderTile = styled.header`
+    padding: 20px;
 
-const DarkGreyTerrazzoWrapper = styled.div`
-    position: absolute;
-    width: 212px;
-    height: 42px;
-    top: 120px;
-    left: 160px;
-`
 
-const DarkGreyTerrazzo2 = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
     font-family: var(--title-m-semibold-font-family);
     font-weight: var(--title-m-semibold-font-weight);
     color: var(--base-900);
@@ -34,94 +25,80 @@ const DarkGreyTerrazzo2 = styled.div`
     font-style: var(--title-m-semibold-font-style);
 `
 
-const Group46 = styled.div`
-    position: absolute;
-    width: 544px;
-    height: 80px;
-    top: 338px;
-    left: 160px;
+
+
+const ProductItem = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 15px;
 `
 
-const TextWrapper42 = styled.div`
-    position: absolute;
-    top: -1px;
-    left: 96px;
-    font-family: var(--title-s-semibold-font-family);
-    font-weight: var(--title-s-semibold-font-weight);
-    color: var(--base-900);
-    font-size: var(--title-s-semibold-font-size);
-    letter-spacing: var(--title-s-semibold-letter-spacing);
-    line-height: var(--title-s-semibold-line-height);
-    white-space: nowrap;
-    font-style: var(--title-s-semibold-font-style);
-`
-
-const TextWrapper43 = styled.div`
-    position: absolute;
-    width: 64px;
-    top: -1px;
-    left: 480px;
-    font-family: var(--title-s-semibold-font-family);
-    font-weight: var(--title-s-semibold-font-weight);
-    color: var(--green-500);
-    font-size: var(--title-s-semibold-font-size);
-    text-align: center;
-    letter-spacing: var(--title-s-semibold-letter-spacing);
-    line-height: var(--title-s-semibold-line-height);
-    font-style: var(--title-s-semibold-font-style);
-`
-
-
-
-const ElementFlat = styled.img`
-    position: absolute;
-    width: 55px;
-    height: 55px;
-    top: 13px;
-    left: 13px;
+const Image = styled.img`
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
     object-fit: cover;
 `
-
-const TextWrapper44 = styled.p`
-    width: 352px;
-    top: 38px;
-    left: 96px;
-    font-weight: var(--text-s-regular-font-weight);
-    color: var(--base-700);
-    font-size: var(--text-s-regular-font-size);
-    line-height: var(--text-s-regular-line-height);
-    position: absolute;
-    font-family: var(--text-s-regular-font-family);
-    letter-spacing: var(--text-s-regular-letter-spacing);
-    font-style: var(--text-s-regular-font-style);
+const Description= styled.div`
+display:flex
+flex-direction:row;;
 `
 
+const Price = styled.div`
+
+
+        font-family: var(--title-s-semibold-font-family);
+        font-weight: var(--title-s-semibold-font-weight);
+        color: var(--green-500);
+        font-size: var(--title-s-semibold-font-size);
+        text-align: center;
+        letter-spacing: var(--title-s-semibold-letter-spacing);
+        line-height: var(--title-s-semibold-line-height);
+        font-style: var(--title-s-semibold-font-style);
+`
+
+const Name = styled.div`
+    font-family: var(--title-s-semibold-font-family);
+        font-weight: var(--title-s-semibold-font-weight);
+        color: var(--base-900);
+        font-size: var(--title-s-semibold-font-size);
+        letter-spacing: var(--title-s-semibold-letter-spacing);
+        line-height: var(--title-s-semibold-line-height);
+        white-space: nowrap;
+        font-style: var(--title-s-semibold-font-style);
+`
+
+const Detail = styled.div`
+        font-weight: var(--text-s-regular-font-weight);
+        color: var(--base-700);
+        font-size: var(--text-s-regular-font-size);
+        line-height: var(--text-s-regular-line-height);
+        position: absolute;
+        font-family: var(--text-s-regular-font-family);
+        letter-spacing: var(--text-s-regular-letter-spacing);
+        font-style: var(--text-s-regular-font-style);
+`
 
 
 const Products = () => {
     return (
-        <>
+        
             <OurProducts>
-                <DarkGreyTerrazzoWrapper>
-                    <DarkGreyTerrazzo2>#Our products</DarkGreyTerrazzo2>
-                </DarkGreyTerrazzoWrapper>
-                <Group46>
-                    <TextWrapper42>Expresso coffee</TextWrapper42>
-                    <TextWrapper43>$9.99</TextWrapper43>
+                <HeaderTile>#Our products</HeaderTile>
 
-                        <ElementFlat
-                            src={ProductPlaceholder}
-                            alt="Expresso coffee"
-                        />
-
-                    <TextWrapper44>
-                        Lorem ipsum dolor sit amet consectetur. Acondiment mi
-                        velit imperdiet cras cras in
-                    </TextWrapper44>
-                </Group46>
-                
+                <ProductItem>
+                    <Image src={ProductPlaceholder} alt="Expresso coffee" />
+                    <Description>
+                        <Name>Expresso coffee</Name>
+                        <Detail>
+                            Lorem ipsum dolor sit amet consectetur. Acondiment
+                            mi velit imperdiet cras cras in
+                        </Detail>
+                    </Description>
+                    <Price>$9.99</Price>
+                </ProductItem>
             </OurProducts>
-        </>
+
     )
 }
 
