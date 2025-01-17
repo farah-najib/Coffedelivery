29b9ee610dc8c38c462c23bba625fadb3b6cc50f
Farah Najib <farahengnajib@gmail.com>
2025-01-17 17:34:35 +0100

    refactor choice component

--- front-end/src/components/Choice.jsx
+++ front-end/src/components/Choice.jsx
@@ -1,362 +1,116 @@
 import styled from 'styled-components'
-
+import ColdBeverage from '../assets/coldbeverage.png'
+import VectorFour from '../assets/vectors/vector4.png'
+import VectorFive from '../assets/vectors/vector5.png'
 const WhereverYouAre3 = styled.div`
-    height: 736px;
-
-    background-color: beige;
-    position: reletive;
-    width: 1440px;
-
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    background-color:#000000;
     background-size: cover;
     background-position: 50% 50%;
 `
 
-const Group31 = styled.div`
-    position: relative;
-    width: 1122px;
-    height: 545px;
-    top: 120px;
-    left: 160px;
-`
-
-const TextWrapper35 = styled.div`
-    left: 1px;
-    font-size: var(--title-m-semibold-font-size);
-    line-height: var(--title-m-semibold-line-height);
-    white-space: nowrap;
-    position: absolute;
-    top: 0;
+const ComponentTitle = styled.div`
+    padding: 20px;
     font-family: var(--title-m-semibold-font-family);
     font-weight: var(--title-m-semibold-font-weight);
     color: var(--basewhite);
+    font-size: var(--title-m-semibold-font-size);
     letter-spacing: var(--title-m-semibold-letter-spacing);
+    line-height: var(--title-m-semibold-line-height);
+    white-space: nowrap;
     font-style: var(--title-m-semibold-font-style);
 `
 
-const Overlap10 = styled.div`
-    position: absolute;
-    width: 1120px;
-    height: 445px;
-    top: 100px;
-    left: 0;
-`
-
-const Group32 = styled.div`
-    position: absolute;
-    width: 1120px;
-    height: 445px;
-    top: 0;
-    left: 0;
-`
-
-const UnsplashLWrapper = styled.div`
-    position: absolute;
-    width: 525px;
-    height: 445px;
-    top: 0;
-    left: 297px;
-    background-size: 100% 100%;
-`
-
-const UnsplashL = styled.img`
-    position: absolute;
-    width: 191px;
-    height: 374px;
-    top: 22px;
-    left: 167px;
-`
-
-const Group33 = styled.div`
-    position: absolute;
-    width: 260px;
-    height: 75px;
-    top: 27px;
-    left: 864px;
-`
-
-const TextWrapper36 = styled.div`
-    width: 255px;
-    left: 1px;
-    font-size: var(--title-s-semibold-font-size);
-    text-align: right;
-    line-height: var(--title-s-semibold-line-height);
-    position: absolute;
-    top: 0;
-    font-family: var(--title-s-semibold-font-family);
-    font-weight: var(--title-s-semibold-font-weight);
-    color: var(--basewhite);
-    letter-spacing: var(--title-s-semibold-letter-spacing);
-    font-style: var(--title-s-semibold-font-style);
-`
-
-const TextWrapper37 = styled.p`
-    position: absolute;
-    width: 256px;
-    top: 39px;
-    left: 0;
-    font-family: var(--text-s-regular-font-family);
-    font-weight: var(--text-s-regular-font-weight);
-    color: var(--base-400);
-    font-size: var(--text-s-regular-font-size);
-    text-align: right;
-    letter-spacing: var(--text-s-regular-letter-spacing);
-    line-height: var(--text-s-regular-line-height);
-    font-style: var(--text-s-regular-font-style);
-`
-
-const Group34 = styled.div`
-    width: 259px;
-    top: 174px;
-    left: 864px;
-    position: absolute;
-    height: 75px;
-`
-
-const TextWrapper38 = styled.div`
-    width: 255px;
-    left: 0;
-    font-size: var(--title-s-semibold-font-size);
-    text-align: right;
-    line-height: var(--title-s-semibold-line-height);
-    position: absolute;
-    top: 0;
-    font-family: var(--title-s-semibold-font-family);
-    font-weight: var(--title-s-semibold-font-weight);
-    color: var(--basewhite);
-    letter-spacing: var(--title-s-semibold-letter-spacing);
-    font-style: var(--title-s-semibold-font-style);
-`
-
-const TextWrapper39 = styled.p`
-    width: 255px;
-    text-align: right;
-    position: absolute;
-    top: 39px;
-    left: 0;
-    font-family: var(--text-s-regular-font-family);
-    font-weight: var(--text-s-regular-font-weight);
-    color: var(--base-400);
-    font-size: var(--text-s-regular-font-size);
-    letter-spacing: var(--text-s-regular-letter-spacing);
-    line-height: var(--text-s-regular-line-height);
-    font-style: var(--text-s-regular-font-style);
-`
-
-const Group35 = styled.div`
-    width: 258px;
-    top: 174px;
-    left: 0;
-    position: absolute;
-    height: 75px;
-`
-
-const Group36 = styled.div`
-    width: 258px;
-    top: 321px;
-    left: 0;
-    position: absolute;
-    height: 75px;
-`
-
-const ChooseYourCoffeeWrapper = styled.div`
-    width: 254px;
-    top: 27px;
-    left: 0;
-    position: absolute;
-    height: 75px;
-`
-
-const ChooseYourCoffee = styled.div`
-    height: 75px;
-`
 
 const Description = styled.div`
-    position: relative;
-    width: 258px;
-    height: 75px;
-`
-
-const Group37 = styled.div`
-    position: absolute;
-    width: 259px;
-    height: 75px;
-    top: 321px;
-    left: 864px;
-`
-
-const Group38 = styled.div`
-    position: absolute;
+    //background: yellow;
     width: 1120px;
-    height: 374px;
-    top: 22px;
-    left: 0;
+    height: 445px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    grid-column-gap: 5px;
+    grid-row-gap: 7px;
+
 `
 
-const Group39 = styled.img`
-    position: absolute;
-    width: 58px;
-    height: 54px;
-    top: 13px;
-    left: 678px;
-`
+const GridRow =styled.div`
+      height:100%;
+      display: flex;
+      flex-direction:column;
+      align-items: center;
+      justify-content: center;
 
-const Group40 = styled.div`
-    width: 260px;
-    top: 5px;
-    left: 864px;
-    position: absolute;
-    height: 75px;
-`
+      color: white;
+      font-size: 20px;
 
-const Group41 = styled.div`
-    width: 259px;
-    top: 152px;
-    left: 864px;
-    position: absolute;
-    height: 75px;
 `
+const StepDescription=styled.div``
 
-const Group42 = styled.div`
-    width: 258px;
-    top: 152px;
-    left: 0;
-    position: absolute;
-    height: 75px;
+
+const Column1 = styled.div`
+
+    height: 100%;
+    //background: blue;
 `
-
-const Group43 = styled.div`
-    width: 258px;
-    top: 299px;
-    left: 0;
-    position: absolute;
-    height: 75px;
+const Column2 = styled.div`
+    height: 100%;
+    //background: rgb(175, 76, 149);
 `
+const Column3 = styled.div`
+    height: 100%;
+    //background: green;
 
-const Group44 = styled.div`
-    width: 254px;
-    top: 5px;
-    left: 0;
-    position: absolute;
-    height: 75px;
 `
-
-const Group45 = styled.div`
-    position: absolute;
-    width: 259px;
-    height: 75px;
-    top: 299px;
-    left: 864px;
-`
-
-const UnsplashLSmblns = styled.img`
-    position: absolute;
+const ColdBeverageImg = styled.img`
     width: 191px;
     height: 374px;
-    top: 0;
-    left: 464px;
 `
+
+const column1Data = [
+    'Which size? Our coffees are carefully selected for quality, flavor, and social impact',
+    'Try our special blends. Our coffees are carefully selected for quality, flavor, and social impact',
+    'Check out our menu. Our coffees are carefully selected for quality, flavor, and social impact'
+];
+const column3Data = [
+    'Upgrade your coffee. Our coffees are carefully selected for quality, flavor, and social impact',
+    'Variety of milk. Our coffees are carefully selected for quality, flavor, and social impact',
+    'Do you need more one? Our coffees are carefully selected for quality, flavor, and social impact'
+];
+
+
 const Choice = () => {
     return (
         <>
             <WhereverYouAre3>
-                <Group31>
-                    <TextWrapper35>#variety of choices</TextWrapper35>
-                    <Overlap10>
-                        <Group32>
-                            <UnsplashLWrapper>
-                                <UnsplashL src="https://c.animaapp.com/EXuBewV4/img/unsplash-l-sm1b4l1ns@2x.png" />
-                            </UnsplashLWrapper>
-                            <Group33>
-                                <TextWrapper36>
-                                    Upgrade your coffee
-                                </TextWrapper36>
-                                <TextWrapper37>
-                                    Our coffees are carefully selected for
-                                    quality, flavor, and social impact
-                                </TextWrapper37>
-                            </Group33>
-                            <Group34>
-                                <TextWrapper38>Variety of milk</TextWrapper38>
-                                <TextWrapper39>
-                                    Our coffees are carefully selected for
-                                    quality, flavor, and social impact
-                                </TextWrapper39>
-                            </Group34>
-                            <Group35>
-                                Which size? Our coffees are carefully selected
-                                for quality, flavor, and social impact
-                            </Group35>
-                            <Group36>
-                                Try our special blends Our coffees are carefully
-                                selected for quality, flavor, and social impact
-                            </Group36>
-                            <ChooseYourCoffeeWrapper>
-                                <ChooseYourCoffee>
-                                    <Description>
-                                        Check out our menu Our coffees are
-                                        carefully selected for quality, flavor,
-                                        and social impact
-                                    </Description>
-                                </ChooseYourCoffee>
-                            </ChooseYourCoffeeWrapper>
-                            <Group37>
-                                <TextWrapper39>
-                                    Our coffees are carefully selected for
-                                    quality, flavor, and social impact
-                                </TextWrapper39>
-                                <TextWrapper38>
-                                    Do you need more one?
-                                </TextWrapper38>
-                            </Group37>
-                        </Group32>
-                        <Group38>
-                            <Group39 src="https://c.animaapp.com/EXuBewV4/img/group-46-1@2x.png" />
-                            <Group40>
-                                <TextWrapper36>
-                                    Upgrade your coffee
-                                </TextWrapper36>
-                                <TextWrapper37>
-                                    Our coffees are carefully selected for
-                                    quality, flavor, and social impact
-                                </TextWrapper37>
-                            </Group40>
-                            <Group41>
-                                <TextWrapper38>Variety of milk</TextWrapper38>
-                                <TextWrapper39>
-                                    Our coffees are carefully selected for
-                                    quality, flavor, and social impact
-                                </TextWrapper39>
-                            </Group41>
-                            <Group42>
-                                Which size? Our coffees are carefully selected
-                                for quality, flavor, and social impact
-                            </Group42>
-                            <Group43>
-                                Try our special blends Our coffees are carefully
-                                selected for quality, flavor, and social impact
-                            </Group43>
-                            <Group44>
-                                <ChooseYourCoffee>
-                                    <Description>
-                                        Check out our menu Our coffees are
-                                        carefully selected for quality flavorand
-                                        social impact
-                                    </Description>
-                                </ChooseYourCoffee>
-                            </Group44>
-                            <Group45>
-                                <TextWrapper39>
-                                    Our coffees are carefully selected for
-                                    quality, flavor, and social impact
-                                </TextWrapper39>
-                                <TextWrapper38>
-                                    Do you need more one?
-                                </TextWrapper38>
-                            </Group45>
-                            <UnsplashLSmblns src="https://c.animaapp.com/EXuBewV4/img/unsplash-l-sm1b4l1ns-1@2x.png" />
-                        </Group38>
-                    </Overlap10>
-                </Group31>
+                <ComponentTitle>#variety of choices</ComponentTitle>
+                <Description>
+                    <Column1>
+                        <GridRow>
+                            {column1Data.map((text, index) => (
+                                <StepDescription key={index}>
+                                    {text}
+                                </StepDescription>
+                            ))}
+                        </GridRow>
+                    </Column1>
+                    <Column2>
+                        <GridRow>
+                            <ColdBeverageImg src={ColdBeverage} />
+                        </GridRow>
+                    </Column2>
+                    <Column3>
+                        <GridRow>
+                            {column3Data.map((text, index) => (
+                                <StepDescription key={index}>
+                                    {text}
+                                </StepDescription>
+                            ))}
+                        </GridRow>
+                    </Column3>
+                </Description>
             </WhereverYouAre3>
         </>
     )
