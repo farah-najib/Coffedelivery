374dc240a2a45e8a859830efa6878ec8628125d0
Farah Najib <farahengnajib@gmail.com>
2025-01-17 14:41:13 +0100

    refactor instagram component

--- front-end/src/components/InstaComp.jsx
+++ front-end/src/components/InstaComp.jsx
@@ -3,66 +3,71 @@ import Instagram1 from '../assets/instagram/instagram1.png'
 import Instagram2 from '../assets/instagram/instagram2.png'
 import Instagram3 from '../assets/instagram/instagram3.png'
 import Instagram4 from '../assets/instagram/instagram4.png'
+
+const instagramImages = {
+    Instagram1,
+    Instagram2,
+    Instagram3,
+    Instagram4
+}
 const Instagram = styled.div`
     position: relative;
-    width: 1440px;
-    height: 780px;
+    width: 100vw;
+    height: 100vh;
+    background-color: pink;
+    background-size: cover;
+    background-position: 50% 50%;
+
+
 `
 
-const Group56 = styled.div`
-    position: relative;
-    width: 1120px;
-    height: 540px;
-    top: 120px;
-    left: 160px;
+
+const ListOfPost = styled.div`
+     position: relative;
+    // width: 1120px;
+    // height: 540px;
+    // display: grid;
+
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(2, auto);
+    gap: 10px;
+    width: 100%;
+    max-width: 800px;
+
 `
 
-const UnsplashImage = styled.img`
-    position: absolute;
+const Post = styled.img`
+    // position: absolute;
     width: 254px;
     height: 254px;
     object-fit: cover;
-    top: ${(props) => props.top || '0'};
-    left: ${(props) => props.left || '0'};
+    // top: ${(props) => props.top || '0'};
+    // left: ${(props) => props.left || '0'};
 `
 
-const IconoirInstagramWrapper = styled.div`
-    position: absolute;
-    width: 254px;
-    height: 254px;
-    top: 0;
-    left: 288px;
-    background-image: url(${Instagram4});
-    background-size: cover;
 
-    background-position: 50% 50%;
-`
 
-const IconoirInstagram = styled.i`
-    position: absolute;
-    width: 108px;
-    height: 108px;
-    top: 73px;
-    left: 73px;
-`
 const InstaComp = () => {
+
+           const imageKeys = [
+               'Instagram1',
+               'Instagram2',
+               'Instagram3',
+               'Instagram4',
+               'Instagram1',
+               'Instagram2',
+               'Instagram3',
+               'Instagram4'
+           ]
     return (
         <>
             <Instagram>
-                <Group56>
-                    <UnsplashImage src={Instagram1} />
-                    <UnsplashImage src={Instagram1} top="286px" />
-                    <UnsplashImage src={Instagram2} top="286px" left="288px" />
-                    <UnsplashImage src={Instagram3} top="0" left="574px" />
-                    <UnsplashImage src={Instagram3} top="286px" left="574px" />
-                    <UnsplashImage src={Instagram4} top="0" left="866px" />
-                    <UnsplashImage src={Instagram4} top="286px" left="866px" />
-                    <IconoirInstagramWrapper>
-                        <IconoirInstagram
-                            className='fi fi-brands-instagram'
-                        />
-                    </IconoirInstagramWrapper>
-                </Group56>
+                <ListOfPost>
+                    {imageKeys.map((key, index) => (
+                        <Post key={index} src={instagramImages[key]} />
+                    ))}
+                </ListOfPost>
             </Instagram>
         </>
     )
