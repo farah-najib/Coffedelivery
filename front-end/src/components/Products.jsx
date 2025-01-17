9b23f122d3defc6016edca427b66df5dc6b5cf9a
Farah Najib <farahengnajib@gmail.com>
2025-01-17 13:59:19 +0100

    add product object to invoke product item

--- front-end/src/components/Products.jsx
+++ front-end/src/components/Products.jsx
@@ -86,21 +86,32 @@ const Detail = styled.div`
 
 
 const Products = () => {
+
+     const product = {
+         id: 'expressoCoffee',
+         name: 'Expresso Coffee',
+         description:
+             'Lorem ipsum dolor sit amet consectetur. Acondiment mi velit imperdiet cras cras in',
+         price: 9.99,
+         currency: 'USD',
+         quantity: 6
+     }
     return (
         <OurProducts>
             <ComponentTitle>#Our products</ComponentTitle>
 
-            <ProductItem>
-                <Image src={ProductPlaceholder} alt="Expresso coffee" />
-                <Description>
-                    <Name>Expresso coffee</Name>
-                    <Detail>
-                        Lorem ipsum dolor sit amet consectetur. Acondiment mi
-                        velit imperdiet cras cras in
-                    </Detail>
-                </Description>
-                <Price>$9.99</Price>
-            </ProductItem>
+            <ProductList>
+                {Array.from({ length: product.quantity }).map((_, index) => (
+                    <ProductItem key={`${product.id}-${index}`}>
+                        <Image src={ProductPlaceholder} alt={product.name} />
+                        <Description>
+                            <Name>{product.name}</Name>
+                            <Detail>{product.description}</Detail>
+                        </Description>
+                        <Price>{`$${product.price.toFixed(2)}`}</Price>
+                    </ProductItem>
+                ))}
+            </ProductList>
         </OurProducts>
     )
 }
