aa882d0fc70e788443a91ae913a4ba6b1d1dddab
Farah Najib <farahengnajib@gmail.com>
2025-01-18 22:17:08 +0100

    refactor schedual component

--- front-end/src/components/Schedule.jsx
+++ front-end/src/components/Schedule.jsx
@@ -3,8 +3,7 @@ import Gallary3 from '../assets/gallary/gallary3.png'
 import Arrow4 from '../assets/vectors/arrow4.png'
 
 import styled from 'styled-components'
-
-const Container = styled.div`
+const ScheduleComponent = styled.div`
     position: relative;
     width: 100vw;
     height: 100vh;
@@ -25,148 +24,103 @@ const ComponentTitle = styled.div`
     font-style: var(--title-m-semibold-font-style);
 `
 
-const Group27 = styled.div`
+const Container = styled.div`
+    height: 100%;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    grid-column-gap: 5px;
+    grid-row-gap: 7px;
+`
+
+const LeftSection = styled.div`
+
     position: relative;
-    width: 1496px;
-    height: 725px;
-    top: 76px;
+    display: flex;
+    flex-direction: row;
+    //background: blue;
 `
-
-const Overlap9 = styled.div`
-    position: relative;
-    width: 1490px;
-    height: 725px;
-`
-
-const DarkGreyTerrazzo = styled.div`
-    left: 160px;
-    position: absolute;
-    top: 0;
-    font-family: var(--title-m-semibold-font-family);
-    font-weight: var(--title-m-semibold-font-weight);
-    color: var(--basewhite);
-    font-size: var(--title-m-semibold-font-size);
-    letter-spacing: var(--title-m-semibold-letter-spacing);
-    line-height: var(--title-m-semibold-line-height);
-    white-space: nowrap;
-    font-style: var(--title-m-semibold-font-style);
-`
-
-
-
-const GallaryImg1 = styled.img`
-    position: absolute;
+const Img1 = styled.div`
     width: 344px;
-
-
-    object-fit: cover;
+    height: 548px;
+    background: url(${Gallary1}) no-repeat center center;
+    background-size: cover;
 `
 
-const GallaryImg2 = styled.img`
-    position: absolute;
+const RightSection = styled.div`
 
-    top: 0;
-    left: 1198px;
+    flex-direction: row-reverse;
+    color: white;
+    //background: pink;
+    position: relative;
+`
+const Img2 = styled.div`
+    width: 292px;
+    height: 489px;
+    background: url(${Gallary3}) no-repeat center center;
+    background-size: cover;
 `
 
-
-
-
-
-const GrabACupOfCoffee = styled.p`
-    position: absolute;
-    width: 637px;
-    top: 0;
-    left: 0;
-    font-family: var(--title-m-semibold-font-family);
-    font-weight: var(--title-m-semibold-font-weight);
-    color: var(--basewhite);
-    font-size: var(--title-m-semibold-font-size);
-    letter-spacing: var(--title-m-semibold-letter-spacing);
-    line-height: var(--title-m-semibold-line-height);
-    font-style: var(--title-m-semibold-font-style);
+const Hashtag = styled.div`
+    font-size: 48px;
+    font-weight: bold;
+    color: #002451;
+    writing-mode: vertical-lr;
+    text-transform: uppercase;
 `
 
-const TextWrapper32 = styled.p`
-    width: 552px;
-    top: 100px;
-    left: 0;
-    font-weight: var(--text-m-regular-font-weight);
-    color: var(--base-500);
-    font-size: var(--text-m-regular-font-size);
-    line-height: var(--text-m-regular-line-height);
-    position: absolute;
-    font-family: var(--text-m-regular-font-family);
-    letter-spacing: var(--text-m-regular-letter-spacing);
-    font-style: var(--text-m-regular-font-style);
+const MiddletSection = styled.div``
+
+const Heading = styled.h1`
+    font-size: 36px;
+    font-weight: bold;
+    margin-bottom: 20px;
 `
 
-const AtomCTAButton2 = styled.div`
-    top: 210px;
-    left: 0;
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    gap: 10px;
-    padding: 10px 24px;
-    position: absolute;
-    background-color: var(--basewhite);
-    border-radius: 8px;
+const Text = styled.p`
+    font-size: 16px;
+    line-height: 1.5;
+    margin-bottom: 30px;
 `
 
-const TextWrapper33 = styled.div`
-    position: absolute;
-    top: 240px;
-    left: 240px;
-    transform: rotate(90deg);
-    font-family: 'Ruda', Helvetica;
-    font-weight: 700;
-    color: var(--base-800);
-    font-size: 58.7px;
-    letter-spacing: 0;
-    line-height: 76.3px;
-    white-space: nowrap;
+const Button = styled.button`
+    background-color: white;
+    color: #ba2b1a;
+    font-size: 16px;
+    padding: 15px 30px;
+    border: none;
+    cursor: pointer;
+    font-weight: bold;
+    border-radius: 5px;
 `
-
-const TextWrapper34 = styled.div`
-    position: absolute;
-    top: 354px;
-    left: 1059px;
-    transform: rotate(-90deg);
-    font-family: 'Ruda', Helvetica;
-    font-weight: 700;
-    color: var(--base-800);
-    font-size: 58.7px;
-    letter-spacing: 0;
-    line-height: 76.3px;
-    white-space: nowrap;
-`
-
-const Group30 = styled.img`
-    position: absolute;
-    width: 212px;
-    height: 193px;
-    top: 532px;
-    left: 248px;
-`
-
 const Schedule = () => {
     return (
-        <Container>
+        <ScheduleComponent>
             <ComponentTitle>#schedule your order</ComponentTitle>
-            <GallaryImg1 src={Gallary1} />
-            <GallaryImg2 src={Gallary3} />
-            Grab a cup of coffee with your buds
-            <br />
-            or at the office. Lorem ipsum dolor sit amet consectetur. Justo
-            faucibus fusce urna faucibus. Blandit sociis pulvinar habitasse
-            aliquam arcu massa at mi consectetur. Faucibus ut elementum fames
-            mauris amet pulvinar ultrices ut sed. Gravida ornare proin eleifend
-            pulvinar ultrices sed in in
-            <p>ORDER NOW AND SCHEDULE YOUR DELIVERY</p>
-            #FRIENDS #WORK
-            <Group30 src={Arrow4} />
-        </Container>
+            <Container>
+                <LeftSection>
+                    <Img1 />
+                    <Hashtag>#Friends</Hashtag>
+                </LeftSection>
+
+                <MiddletSection>
+                    <Heading>
+                        Grab a cup of coffee with your buds or at the office.
+                    </Heading>
+                    <Text>
+                        Lorem ipsum dolor sit amet consectetur. Justo faucibus
+                        fusce urna faucibus. Blandit sociis pulvinar habitasse
+                        aliquam arcu massa at mi consectetur.
+                    </Text>
+                    <Button>Order Now and Schedule Your Delivery</Button>
+                </MiddletSection>
+
+                <RightSection>
+                    <Img2 />
+                    <Hashtag>#Work</Hashtag>
+                </RightSection>
+            </Container>
+        </ScheduleComponent>
     )
 }
 export default Schedule
