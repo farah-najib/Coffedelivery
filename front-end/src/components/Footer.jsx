4d5861c2419dad8feb542fb00c00ce5ec46e2c46
Farah Najib <farahengnajib@gmail.com>
2025-01-19 14:55:39 +0100

    refactor footer

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -1,360 +1,133 @@
 
 import styled from 'styled-components'
+import BackgroundImg from '../assets/bkfooter.png'
+import Logo from '../assets/logo-img/logo.png'
 
-const FooterWrapper = styled.footer`
+
+const FooterContainer = styled.footer`
     position: relative;
     width: 1441px;
     height: 533px;
-    background-color: transparent;
+    background-image: url(${BackgroundImg});
+    color: #fff;
+    padding: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    font-family: 'Arial', sans-serif;
 `
 
-const OrganismFooter = styled.div`
-    width: 1441px;
-    height: 533px;
+const Column = styled.div`
+    flex: 1;
+    margin: 10px;
+    min-width: 200px;
+
+    h3 {
+        font-size: 18px;
+        margin-bottom: 10px;
+        font-weight: bold;
+    }
+
+    p,
+    a {
+        font-size: 14px;
+        color: #aaa;
+        text-decoration: none;
+        margin-bottom: 5px;
+        display: block;
+    }
+
+    a:hover {
+        color: #fff;
+    }
 `
 
-const Overlap12 = styled.div`
-    position: relative;
-    height: 541px;
-    top: -8px;
-    background-image: url(https://c.animaapp.com/EXuBewV4/img/rectangle-24-2.png);
-    background-size: 100% 100%;
+const SocialIcons = styled.div`
+    display: flex;
+    gap: 10px;
+
+    a {
+        font-size: 20px;
+        color: #fff;
+        transition: color 0.3s;
+    }
 `
 
-const TextWrapper49 = styled.div`
-    position: absolute;
-    top: 198px;
-    left: 1028px;
-    font-family: var(--title-s-semibold-font-family);
-    font-weight: var(--title-s-semibold-font-weight);
-    color: #ffffff;
-    font-size: var(--title-s-semibold-font-size);
-    letter-spacing: var(--title-s-semibold-letter-spacing);
-    line-height: var(--title-s-semibold-line-height);
-    white-space: nowrap;
-    font-style: var(--title-s-semibold-font-style);
+const NewsletterContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap:1rem;
+    width:259px;
+    height:97px;
 `
-
-const Group61 = styled.div`
-    position: absolute;
-    width: 259px;
-    height: 97px;
-    top: 246px;
-    left: 1026px;
-`
-
-const AtomInput = styled.div`
-    position: absolute;
+const Input = styled.input`
     width: 259px;
     height: 42px;
-    top: 0;
-    left: 0;
-    border-radius: 8px;
-`
-
-const PlaceholderWrapper = styled.div`
-    display: flex;
-    width: 259px;
-    align-items: center;
+    // align-items: center;
     gap: 10px;
-    padding: 12px 16px;
-    position: relative;
     border-radius: 8px;
     border: 1px solid;
     border-color: var(--base-400);
 `
 
-const Placeholder = styled.div`
-    position: relative;
-    width: fit-content;
-    margin-top: -1px;
-    font-family: var(--text-s-regular-font-family);
-    font-weight: var(--text-s-regular-font-weight);
-    color: var(--base-500);
-    font-size: var(--text-s-regular-font-size);
-    letter-spacing: var(--text-s-regular-letter-spacing);
-    line-height: var(--text-s-regular-line-height);
-    white-space: nowrap;
-    font-style: var(--text-s-regular-font-style);
-`
-
-const AtomPrimaryButton2 = styled.div`
-    gap: 4px;
-    padding: 12px 24px;
-    top: 55px;
-    left: 0;
-    background-color: var(--basewhite);
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    border-radius: 8px;
-`
-
-const TextWrapper50 = styled.div`
-    color: var(--base-900);
-    font-size: var(--text-s-bold-font-size);
-    line-height: var(--text-s-bold-line-height);
-    position: relative;
-    width: fit-content;
-    margin-top: -1px;
-    font-family: var(--text-s-bold-font-family);
-    font-weight: var(--text-s-bold-font-weight);
-    letter-spacing: var(--text-s-bold-letter-spacing);
-    white-space: nowrap;
-    font-style: var(--text-s-bold-font-style);
-`
-
-const Group62 = styled.div`
-    position: absolute;
-    width: 630px;
-    height: 257px;
-    top: 120px;
-    left: 161px;
-`
-
-const Group63 = styled.div`
-    position: absolute;
-    width: 261px;
-    height: 257px;
-    top: 0;
-    left: 0;
-`
-
-const Group64 = styled.div`
-    position: absolute;
-    width: 96px;
-    height: 66px;
-    top: 0;
-    left: 0;
-`
-
-const TextWrapper51 = styled.div`
-    position: absolute;
-    width: 257px;
-    top: 129px;
-    left: 0;
-    font-family: var(--text-s-regular-font-family);
-    font-weight: var(--text-s-regular-font-weight);
-    color: var(--base-100);
-    font-size: var(--text-s-regular-font-size);
-    letter-spacing: var(--text-s-regular-letter-spacing);
-    line-height: var(--text-s-regular-line-height);
-    font-style: var(--text-s-regular-font-style);
-`
-
-const IconoirInstagram = styled.i`
-    position: absolute;
-    width: 32px;
-    height: 32px;
-    top: 225px;
-    left: 0;
-    background: white;
-`
-
-const IconoirTiktok = styled.i`
-    position: absolute;
-    width: 32px;
-    height: 32px;
-    top: 225px;
-    left: 48px;
-    background:white;
-`
-
-const Group65 = styled.div`
-    position: absolute;
-    width: 32px;
-    height: 32px;
-    top: 225px;
-    left: 96px;
-`
-
-const OverlapGroup8 = styled.div`
-    position: relative;
-    width: 27px;
-    height: 19px;
-    top: 7px;
-    left: 3px;
-`
 
 
+ const Button = styled.button`
+     gap: 4px;
+     padding: 12px 24px;
+     width: 124px;
+     height: 42px;
+     background-color: var(--basewhite);
+     display: inline-flex;
+     align-items: center;
+     justify-content: center;
+     border-radius: 8px;
+     color:black;
+ `
 
-const Vector3 = styled.i`
-    position: absolute;
-    width: 27px;
-    height: 19px;
-    top: 0;
-    left: 0;
-    background: white;
-`
-
-const TextWrapper52 = styled.div`
-    position: absolute;
-    top: 82px;
-    left: 0;
-    font-family: var(--title-s-semibold-font-family);
-    font-weight: var(--title-s-semibold-font-weight);
-    color: #ffffff;
-    font-size: var(--title-s-semibold-font-size);
-    letter-spacing: var(--title-s-semibold-letter-spacing);
-    line-height: var(--title-s-semibold-line-height);
-    white-space: nowrap;
-    font-style: var(--title-s-semibold-font-style);
-`
-
-const Group66 = styled.div`
-    position: absolute;
-    width: 156px;
-    height: 125px;
-    top: 129px;
-    left: 482px;
-`
-
-
-
-
-
-
-
-
-const Group67 = styled.div`
-    position: absolute;
-    width: 134px;
-    height: 175px;
-    top: 82px;
-    left: 289px;
-`
-
-const TextWrapper57 = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-    font-family: var(--title-s-semibold-font-family);
-    font-weight: var(--title-s-semibold-font-weight);
-    color: var(--basewhite);
-    font-size: var(--title-s-semibold-font-size);
-    letter-spacing: var(--title-s-semibold-letter-spacing);
-    line-height: var(--title-s-semibold-line-height);
-    white-space: nowrap;
-    font-style: var(--title-s-semibold-font-style);
-`
-
-const TextWrapper58 = styled.div`
-    top: 47px;
-    position: absolute;
-    left: 0;
-    font-family: var(--text-l-regular-font-family);
-    font-weight: var(--text-l-regular-font-weight);
-    color: var(--base-200);
-    font-size: var(--text-l-regular-font-size);
-    letter-spacing: var(--text-l-regular-letter-spacing);
-    line-height: var(--text-l-regular-line-height);
-    white-space: nowrap;
-    font-style: var(--text-l-regular-font-style);
-`
-
-const TextWrapper59 = styled.div`
-    position: absolute;
-    top: 81px;
-    left: 0;
-    font-family: var(--text-l-regular-font-family);
-    font-weight: var(--text-l-regular-font-weight);
-    color: var(--base-200);
-    font-size: var(--text-l-regular-font-size);
-    letter-spacing: var(--text-l-regular-letter-spacing);
-    line-height: var(--text-l-regular-line-height);
-    white-space: nowrap;
-    font-style: var(--text-l-regular-font-style);
-`
-
-const TextWrapper60 = styled.div`
-    position: absolute;
-    top: 115px;
-    left: 0;
-    font-family: var(--text-l-regular-font-family);
-    font-weight: var(--text-l-regular-font-weight);
-    color: var(--base-200);
-    font-size: var(--text-l-regular-font-size);
-    letter-spacing: var(--text-l-regular-letter-spacing);
-    line-height: var(--text-l-regular-line-height);
-    white-space: nowrap;
-    font-style: var(--text-l-regular-font-style);
-`
-
-const TextWrapper61 = styled.div`
-    position: absolute;
-    top: 149px;
-    left: 0;
-    font-family: var(--text-l-regular-font-family);
-    font-weight: var(--text-l-regular-font-weight);
-    color: var(--base-200);
-    font-size: var(--text-l-regular-font-size);
-    letter-spacing: var(--text-l-regular-letter-spacing);
-    line-height: var(--text-l-regular-line-height);
-    white-space: nowrap;
-    font-style: var(--text-l-regular-font-style);
-`
-
-const Text = styled.div`
-    position: absolute;
-    top: 454px;
-    left: 155px;
-    font-family: var(--text-l-regular-font-family);
-    font-weight: var(--text-l-regular-font-weight);
-    color: #000000;
-    font-size: var(--text-l-regular-font-size);
-    letter-spacing: var(--text-l-regular-letter-spacing);
-    line-height: var(--text-l-regular-line-height);
-    white-space: nowrap;
-    font-style: var(--text-l-regular-font-style);
-`
 
 const Footer = () => {
     return (
-        <FooterWrapper>
-            <OrganismFooter>
-                <Overlap12>
-                    <TextWrapper49>News letter</TextWrapper49>
-                    <Group61>
-                        <AtomInput>
-                            <PlaceholderWrapper>
-                                <Placeholder>example@email.com</Placeholder>
-                            </PlaceholderWrapper>
-                        </AtomInput>
-                        <AtomPrimaryButton2>
-                            <TextWrapper50>Text button</TextWrapper50>
-                        </AtomPrimaryButton2>
-                    </Group61>
-                    <Group62>
-                        <Group63>
-                            <Group64>
-                                <TextWrapper51>Follow us</TextWrapper51>
-                                <IconoirInstagram className="fi fi-brands-instagram" />
-                                <IconoirTiktok className="fi fi-brands-tik-tok" />
-
-                            </Group64>
-                        </Group63>
-                        <Group65>
-                            <OverlapGroup8>
-                           
-                                <Vector3 className="fi fi-brands-youtube" />
-                            </OverlapGroup8>
-                        </Group65>
-                    </Group62>
-                    <Group66>
-                        <TextWrapper52>Contact Us</TextWrapper52>
-                        <Group67>
-                            <TextWrapper57>Location</TextWrapper57>
-                            <TextWrapper58>Email</TextWrapper58>
-                            <TextWrapper59>Phone</TextWrapper59>
-                            <TextWrapper60>Address</TextWrapper60>
-                            <TextWrapper61>Website</TextWrapper61>
-                        </Group67>
-                    </Group66>
-                </Overlap12>
-            </OrganismFooter>
-            <Text>Footer text goes here</Text>
-        </FooterWrapper>
+        <FooterContainer>
+            <Column>
+                <img src={Logo} />
+                <p>
+                    At our company, we strive to make your coffee time more
+                    enjoyable. Since 2020, we have been providing high-quality
+                    coffee and excellent service.
+                </p>
+                <SocialIcons>
+                    <a href="#" aria-label="Instagram">
+                        <i className="fi fi-brands-instagram" />
+                    </a>
+                    <a href="#" aria-label="TikTok">
+                        <i className="fi fi-brands-tik-tok" />
+                    </a>
+                    <a href="#" aria-label="YouTube">
+                        <i className="fi fi-brands-youtube" />
+                    </a>
+                </SocialIcons>
+            </Column>
+            <Column>
+                <h3>Quick Links</h3>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Services</a>
+                <a href="#">Menu</a>
+                <a href="#">Purchase</a>
+                <a href="#">My Account</a>
+                <a href="#">My Orders</a>
+                <a href="#">Favorite Coffees</a>
+            </Column>
+            <Column>
+                <h3>Newsletter</h3>
+                <NewsletterContainer>
+                    <Input type="email" placeholder="example@email.com" />
+                    <Button>Subscribe</Button>
+                </NewsletterContainer>
+            </Column>
+        </FooterContainer>
     )
 }
 
