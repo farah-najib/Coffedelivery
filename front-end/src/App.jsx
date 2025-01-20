639a6882f0496df4926884878cb1cd1c6ca611d1
Farah Najib <farahengnajib@gmail.com>
2025-01-20 21:04:05 +0100

    create styled component application

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,11 +4,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from 'pages/LandingPage'
 import SignInPage from 'pages/LoginFlow'
 import { createGlobalStyle } from 'styled-components'
+import styled from 'styled-components'
 
 const GlobalStyle = createGlobalStyle``
+
+const Application = styled.div`
+overflow:hidden;
+
+`
 function App() {
     return (
-        <div className="app">
+        <Application>
             <GlobalStyle />
             <Router>
                 <Routes>
@@ -16,7 +22,7 @@ function App() {
                     <Route path="/signin" element={<SignInPage />} />
                 </Routes>
             </Router>
-        </div>
+        </Application>
     )
 }
 
