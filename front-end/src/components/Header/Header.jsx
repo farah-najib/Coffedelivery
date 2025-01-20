4a766ecaafa0a4f0baf2ca6173d216479b5803cc
Farah Najib <farahengnajib@gmail.com>
2025-01-20 21:02:09 +0100

    refactor header

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,59 +1,48 @@
-import Logo from 'assets/logo-img/logo1.png'
+
 import { useNavigate } from 'react-router-dom'
-import{NavbarContainer, BrandContainer , NavLinks,CustomButton } from "./Header.styles"
+import { useState } from 'react'
+import {
+    HeaderContainer,
+    HeaderLogo,
+    NavLinks,
+    SigninButton,
+    Link,
+    MenuBtn
+} from './Header.styles'
 
-
-const Navbar = () => {
+const navLinks = [
+    { href: '#Home', label: 'Home' },
+    { href: '#About', label: 'About' },
+    { href: '#services', label: 'Services' },
+    { href: '#menu', label: 'Menu' },
+    { href: '#schedule', label: 'Schedule' }
+]
+const Header = () => {
     const navigate = useNavigate() // Initialize navigate function
 
     const handleSignInClick = () => {
         navigate('/signin') // Navigate to the Sign In page
     }
+    const [menuOpen, setMenuOpen] = useState(false)
     return (
-        <NavbarContainer className="navbar navbar-expand-lg">
-            <div className="container">
-                <a href="#home" className="navbar-brand">
-                    <BrandContainer src={Logo} />
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <NavLinks className="navbar-nav ms-auto">
-                        <a href="#features" className="nav-link">
-                            Home
-                        </a>
-                        <a href="#pricing" className="nav-link">
-                            About
-                        </a>
-                        <a href="#services" className="nav-link">
-                            Services
-                        </a>
-                        <a href="#menu" className="nav-link">
-                            Menu
-                        </a>
-                        <a href="#schedule" className="nav-link">
-                            Schedule
-                        </a>
-                    </NavLinks>
-                    <div>
-                        <i className="fi fi-br-search"></i>
-                        <CustomButton onClick={handleSignInClick}>
-                            Sign in
-                        </CustomButton>
-                    </div>
-                </div>
+        <HeaderContainer>
+            <HeaderLogo href="#home" />
+            <MenuBtn onClick={() => setMenuOpen(!menuOpen)}>
+                <i className="fi fi-br-menu-burger"></i>
+            </MenuBtn>
+            <NavLinks show={menuOpen}>
+                {navLinks.map((link, index) => (
+                    <Link key={index}>
+                        <a href={link.href}>{link.label}</a>
+                    </Link>
+                ))}
+            </NavLinks>
+            <div>
+                <i className="fi fi-br-search"></i>
+                <SigninButton onClick={handleSignInClick}>Sign in</SigninButton>
             </div>
-        </NavbarContainer>
+        </HeaderContainer>
     )
 }
 
-export default Navbar
+export default Header
--- front-end/src/components/Header/Header.styles.js
+++ front-end/src/components/Header/Header.styles.js
@@ -1,27 +1,73 @@
 import styled from 'styled-components'
+import Logo from 'assets/logo-img/logo1.png'
 
-export const  NavbarContainer = styled.nav`
-    // padding: 16px 159px;
-
-    overflow: hidden;
-    //background-color: #fff;
-    //border-radius:50px;
-    position: fixed;
-    top: 0;
-    width: 100%;
-    z-index: 1000;
-`
-
-export const  BrandContainer = styled.img``
-
-export const  NavLinks = styled.div`
+export const HeaderContainer = styled.nav`
+    height: 4.813rem;
+    background-color: #f9f4e6;
     display: flex;
-    gap: 16px;
+    justify-content: space-between;
+    align-items: center;
+`
+export const HeaderLogo = styled.a`
+    display: block;
+    width: 63px;
+    height: 45px;
+    background-image: url(${Logo});
+    background-repeat: no-repeat;
+    background-size: contain;
+    float: left;
+    padding: 10px 20px;
+`
+export const NavLinks = styled.ul`
+    display: flex;
+    list-style: none;
+    @media screen and (max-width: 768px) {
+        position: absolute;
+        top: 50px;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        flex-direction: column;
+        background-color: #252525;
+        align-items: center;
+        display: ${(props) => (props.show ? 'flex' : 'none')};
+        padding-top: 60px;
+    }
 `
 
-export const  CustomButton = styled.button`
+export const Link = styled.li`
+    a {
+        color: black;
+        text-decoration: none;
+        display: inline-block;
+        padding: 10px 20px;
+        &:hover {
+
+        }
+        @media screen and (max-width: 768px) {
+            margin-bottom: 10px;
+            display: block;
+        }
+    }
+`
+
+export const MenuBtn = styled.div`
+    display: none;
+    position: absolute;
+    color: black;
+    top: 10px;
+    right: 30px;
+    font-size: 2rem;
+    cursor: pointer;
+
+    @media screen and (max-width: 768px) {
+        display: block;
+    }
+`
+
+export const  SigninButton = styled.button`
     background-color: var(--green-600);
     border-radius: 8px;
-    color: white;
+    color: black;
     padding: 8px 16px;
 `
