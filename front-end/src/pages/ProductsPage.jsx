fdc981e1855a62043bc2bc931cd18276714c3f82
Farah Najib <farahengnajib@gmail.com>
2025-01-29 10:46:31 +0100

    .

---
+++ front-end/src/pages/ProductsPage.jsx
@@ -0,0 +1,15 @@
+import Header from 'components/Header/Header'
+import Footer from 'components/Footer/Footer'
+
+
+const ProductsPage = () => {
+    return (
+        <div className="landing-page">
+            <Header />
+
+            <Footer />
+        </div>
+    )
+}
+
+export default ProductsPage
