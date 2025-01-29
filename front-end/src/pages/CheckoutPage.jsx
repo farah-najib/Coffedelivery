7e9254360ba3cdaaaa43139feb9e1a0675b18a27
Farah Najib <farahengnajib@gmail.com>
2025-01-29 10:45:55 +0100

    check out page

---
+++ front-end/src/pages/CheckoutPage.jsx
@@ -0,0 +1,31 @@
+
+import Header from 'components/Header/Header'
+import Footer from 'components/Footer/Footer'
+import styled from 'styled-components'
+import CustomMap from 'components/CustomMap'
+import DeliveryDetails from 'src/components/DeliveryDetails'
+import { APIProvider } from '@vis.gl/react-google-maps'
+const ContentWrapper = styled.div`
+    position: relative; /* This ensures child absolute elements stay within this section */
+    width: 100%;
+    height: 100vh; /* Adjust as needed */
+
+    //padding: 50px;
+`
+const CheckoutPage = () => {
+    return (
+        <div className="checkout-page">
+            <Header />
+            <ContentWrapper>
+                <DeliveryDetails/>
+
+                <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+                    <CustomMap />
+                </APIProvider>
+            </ContentWrapper>
+            <Footer />
+        </div>
+    )
+}
+
+export default CheckoutPage
